e83b94f1d244a9950649f9d70545e9499fed3187
Prasad Pawtekar <prasad@rjtcompuquest.com>
2022-07-22 21:27:27 +0530

    Addition operation implemented.

--- app/src/main/java/com/apolisrises/rjtcalcj/MainActivity.kt
+++ app/src/main/java/com/apolisrises/rjtcalcj/MainActivity.kt
@@ -2,6 +2,7 @@ package com.apolisrises.rjtcalcj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.apolisrises.rjtcalcj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener {
+            val a = binding.etN1.text.toString().toInt()
+            val b = binding.etN2.text.toString().toInt()
+            val addition = a + b
+            Toast.makeText(baseContext, "Addition is $addition", Toast.LENGTH_SHORT).show()
+        }
     }
 }
