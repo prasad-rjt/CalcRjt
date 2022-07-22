23eecc27205a9cf8cd1ba658dd0d12d155efb4fb
Prasad Pawtekar <prasad@rjtcompuquest.com>
2022-07-22 21:36:57 +0530

    Multiplication operation is completed.

--- app/src/main/java/com/apolisrises/rjtcalcj/MainActivity.kt
+++ app/src/main/java/com/apolisrises/rjtcalcj/MainActivity.kt
@@ -18,5 +18,12 @@ class MainActivity : AppCompatActivity() {
             val addition = a + b
             Toast.makeText(baseContext, "Addition is $addition", Toast.LENGTH_SHORT).show()
         }
+
+        binding.btnAdd.setOnClickListener {
+            val a = binding.etN1.text.toString().toInt()
+            val b = binding.etN2.text.toString().toInt()
+            val multiplication = a * b
+            Toast.makeText(baseContext, "Addition is $multiplication", Toast.LENGTH_SHORT).show()
+        }
     }
 }
